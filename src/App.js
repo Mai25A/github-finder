690cfb629a2336cfb283ab1d7902f1792d40f30c
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-06-08 21:46:08 +0700

    create search

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// App.js
+import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-// import Search from "./components/users/Search";
-import Users from "./components/users/Users";
-function App() {
-  const [users, setUsers] = useState([]);
-  // Use the 'useEffect' hook to perform side effects in function components
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://api.github.com/users");
-        setUsers(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>GitHub Users Data</h1>
-      </div>
-      <Users users={users} />
-    </div>
-  );
-}
+import Search from "./components/layout/Search";
+const App = () => {
+return (
+<div className="App">
+<Navbar />
+<div className="container">
+<h1>GitHub Users Data</h1>
+<Search />
+</div>
+</div>
+);
+};
 export default App;
